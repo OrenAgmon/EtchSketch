ad029168e4cd2ddabdeaa2e5d1d56dea227ad69d
Oren Agmon <oren.agmon98@gmail.com>
2021-09-27 16:39:54 +0300

    final version for beginner level

--- script.js
+++ script.js
@@ -2,114 +2,124 @@ const container = document.getElementById('grid-container')
 const slider = document.getElementById('slider')
 const sliderValue = document.getElementById('slider-value')
 
-const  greyButton  = document.getElementById('btn-grey')
+const greyButton = document.getElementById('btn-grey')
 const restartButton = document.getElementById('btn-restart')
 const eraserButton = document.getElementById('btn-eraser')
 const rainbowButton = document.getElementById('btn-rainbow')
-const colorPicker = document.getElementById('cell-color')
-
-let gridCells = document.querySelectorAll('.grid-cell')
+const colorPicker = document.getElementById('color-picker')
 
 
 
-greyButton.addEventListener('click', greyOption)
-eraserButton.addEventListener('click', eraserOption)
-restartButton.addEventListener('click', restartOption);
-rainbowButton.addEventListener('click', rainbowOption);
+greyButton.addEventListener('click', greyOption);
+eraserButton.addEventListener('click', eraserBtn);
+restartButton.addEventListener('click', restartBtn);
+rainbowButton.addEventListener('click', rainbowBtn);
+colorPicker.addEventListener('change', colorPickerBtn)
+
+
+let gridCells = document.querySelectorAll('grid-cell')
+
 
 
 newGridSize(10)
 
 
-    slider.onchange= function(){
+slider.onchange = function () {
 
-        let newSize = this.value
-        sliderValue.textContent = `${newSize} X ${newSize}`;
-        newGridSize(newSize)
+  let newSize = this.value
+  sliderValue.textContent = `${newSize} X ${newSize}`;
+  newGridSize(newSize)
 
-    }
-
-
+}
 
 
-  function newGridSize(newSize){
-    gridCells = document.querySelectorAll('.grid-cell')
-    if(gridCells.length != 0){
-      deletePreviousGrid();
-    }
-    
-
-  for(let i=0; i<(newSize*newSize);i++){
-      let newGridCell = document.createElement('div')
-      newGridCell.style.border = '1px solid black'
-      container.appendChild(newGridCell);
-      newGridCell.className = 'grid-cell'
-
-      container.style.gridTemplateColumns = `repeat(${newSize}, auto)`
+function newGridSize(newSize) {
+  gridCells = document.querySelectorAll('.grid-cell')
+  if(gridCells.length != 0){
+    deletePreviousGrid();
   }
-
-    
   
-    }
 
-  function deletePreviousGrid(){
-    GridCells = document.querySelectorAll('.grid-cell')
-    GridCells.forEach(cell => {
-         cell.remove();
+  for (let i = 0; i < (newSize * newSize); i++) {
+    let newGridCell = document.createElement('div')
+    newGridCell.style.border = '1px solid black'
+    newGridCell.style.backgroundColor = 'rgb(255, 255, 255)'
+    container.appendChild(newGridCell);
+    newGridCell.className = 'grid-cell'
+
+    container.style.gridTemplateColumns = `repeat(${newSize}, auto)`
+  }
+
+
+
+}
+
+function deletePreviousGrid() {
+  let GridCells = document.querySelectorAll('.grid-cell')
+  GridCells.forEach(cell => {
+    cell.remove();
+  })
+}
+
+function colorPickerBtn(e){
+  console.log(e);
+  let cells = document.querySelectorAll('.grid-cell')
+  cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+    
+
+
+      cell.style.background = `${e.target.value}`
     })
-  }
+  })
+}
 
 
+function greyOption() {
+  let cells = document.querySelectorAll('.grid-cell');
+
+ 
+
+  cells.forEach(cell => {
+    let greyR = 202;
+    let greyG = 202;
+    let greyB = 202;
+    
+    cell.removeEventListener('mouseover', deleteBackground)
+
+  cell.addEventListener('mouseover', () =>{
+    let cellCurrentColor = getComputedStyle(cell).backgroundColor;
+    console.log(cellCurrentColor);
+     cell.setAttribute('listener', 'true')
+    if(cellCurrentColor == 'rgb(255, 255, 255)'){
+        greyR=202;
+        greyG=202;
+        greyB=202;
+     }
+    
+    cell.style.backgroundColor = `rgb(${greyR-=20},${greyG-=20},${greyB-=20})`
+ 
+  })
+  });
+
+}
 
 
-  function greyOption(){
-    gridCells = document.querySelectorAll('.grid-cell');
+function eraserBtn() {
+  let cells = document.querySelectorAll('.grid-cell');
 
-     gridCells.forEach(cell =>{
-             cell.removeEventListener('mouseover',deleteOption)
-          })
-   
-    gridCells.forEach(cell  => { 
-        let greyR = 202;
-        let greyG = 202;
-        let greyB = 202;
-        cell.addEventListener('mouseover', function makeGrey(){ 
+  cells.forEach(cell => {
+    
+    cell.addEventListener("mouseover", deleteBackground)
+  });
+}
 
-            let cellCurrentColor = getComputedStyle(cell).backgroundColor;
-            console.log(cellCurrentColor);
-
-            if(cellCurrentColor == 'rgb(255, 255, 255)'){
-                greyR=202;
-                greyG=202;
-                greyB=202;
-             }
-            
-            cell.style.background = `rgb(${greyR-=20},${greyG-=20},${greyB-=20})`
-            console.log(greyR);
-            console.log(greyG);
-            console.log(greyB);
-           
-        })
-        
-    });
-
-  }
-
-
-  function deleteOption (e) {
-
-      e.target.style.backgroundColor ="rgb(255, 255, 255)"
-  }
-
-  function eraserOption(){
-     gridCells = document.querySelectorAll('.grid-cell');
-     gridCells.forEach(cell  => { 
-      
-        cell.addEventListener("mouseover",deleteOption)
-    });
-
-  }
-  // when need to remove listeners
+function deleteBackground(e){
+  console.log(e.target)
+  e.target.setAttribute('listener', 'true')
+  e.target.style.backgroundColor = "rgb(255, 255, 255)"
+}
+// when need to remove listeners
 //   let cells = document.querySelectorAll('grid-cell')
 //  
 
@@ -118,70 +128,37 @@ newGridSize(10)
 // function defaultBlackOption(){
 //     let cells = document.querySelectorAll('.grid-cell');
 //     cells.forEach(cell  => { 
-       
+
 //         cell.addEventListener('mouseover', () =>{
 //             cell.style.background = 'black';
 //         })
 
-        
+
 //     });
 // }
 
-function restartOption(){
-    gridCells = document.querySelectorAll('.grid-cell');
-    gridCells.forEach(cell  => { 
-       
-        cell.style.background = 'rgb(255, 255, 255)';
-
-        
-    });
+function restartBtn() {
+  let cells = document.querySelectorAll('.grid-cell');
+  cells.forEach(cell => {
+    cell.style.background = 'rgb(255, 255, 255)';
+  });
 }
 
-function rainbowOption(){
+function rainbowBtn() {
 
-  gridCells = document.querySelectorAll('.grid-cell')
-  gridCells.forEach(cell =>{
-        cell.addEventListener('mouseover', ()=>{
-            let R = Math.floor((Math.random()*255)+1)
-            let G = Math.floor((Math.random()*255)+1)
-            let B = Math.floor((Math.random()*255)+1)
-            
-        
-            cell.style.background = `rgb(${R},${G},${B})`
-        })
+  let cells = document.querySelectorAll('.grid-cell')
+  cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+      let R = Math.floor((Math.random() * 255) + 1)
+      let G = Math.floor((Math.random() * 255) + 1)
+      let B = Math.floor((Math.random() * 255) + 1)
+
+
+      cell.style.background = `rgb(${R},${G},${B})`
     })
-    
+  })
+
 }
-
-// function setColor(option, R, G, B){
-//   gridCells = document.querySelectorAll('.grid-cell')
-//   gridCells.forEach(cell =>{
-//     let startingOpacity = 0
-//     cell.addEventListener('mouseover', ()=>{
-      
-//       switch(option){
-//         case 'grey':
-//           if(cell.style.backgroundColor == 'rgb(255, 255, 255)') startingOpacity = 0; 
-//           cell.style.backgroundColor = `rgba(${R}, ${G}, ${B}, ${startingOpacity+=0.1}`
-//           break;
-//         case 'erase':
-//           cell.style.backgroundColor = 'rgb(255,255,255)'
-//           break;
-        
-
-//       }
-     
-       
-      
-     
-
-        
-
-//       })
-//   })
-
-//   }
-
 
 
 
